921f0b4961c1e2ea76975456b8ae73b263c02026
caio180 <caio.lima.viana2009@gmail.com>
2026-05-22 14:48:54 -0300

    Pegando os dados do frontend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import cors from 'cors'
 const app = express();
-const porta = process.env.PORT || 8080;
+const porta = process.env.PORT;
+app.use(cors())
+app.use(express.json())
+
+const usuarios = []
 
 app.get('/', (req, res) => {
     res.send("Teste");
+})
+app.post('/cadastro', (req, res) =>{
+    usuarios.push(req.body)
+})
+app.get('/cadastro', (req, res) =>{
+    res.json(usuarios)
 })
 
 app.listen(porta, () => console.log(`Servidor rodando: http://localhost:${porta}`))
