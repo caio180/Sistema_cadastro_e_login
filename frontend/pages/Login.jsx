3068be4b103e8786f518d522b01fae4658d2f13a
caio180 <caio.lima.viana2009@gmail.com>
2026-06-24 15:08:02 -0300

    Adicionando a estrutura do formulario de login

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -3,8 +3,35 @@ import { useState } from "react";
 function Login(){
     const [email, setEmail] = useState("");
     const [senha, setSenha] = useState("");
-    const handleLogin = (e) => {
+    const regis_Login = (e) => {
         e.preventDefalt()
+        
     }
     
+    return (<>
+        <div id="login">
+        <h2>tela login</h2>
+        
+        <form id="formLogin" onSubmit={regis_Login}>
+            
+            <input 
+                type="email"
+                placeholder="Digite seu email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Digite sua senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+            />
+            <button id="bo_login" type="submit">Login</button>
+        </form>
+    </div> 
+    </>);
+
+    
+    
 }
+export default Login;
